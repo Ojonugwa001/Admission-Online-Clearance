3650e8c1cae8fb502f20e47edb8b122145277a00
Alikali Ojonugwa <ojonugwajusticealikali@gmail.com>
2019-02-26 15:40:48 +0100

    Update status in DB

--- controllers/home.js
+++ controllers/home.js
@@ -23,6 +23,7 @@ module.exports = function(app) {
 
 	          		Profile.authenticate(req.session.userId, function (error, profileUserId) {
 	          			var email = req.session.userEmail;
+	      				
 	      				if (error || !profileUserId) {
 	      					Profile.find({},'userId firstname lastname department approved', function(err, profileData){
 	        					if (err) {
@@ -44,7 +45,7 @@ module.exports = function(app) {
 						        return callback(err);
 						      }
 						      var name = user.lastname + " " + user.firstname;
-						      return res.render('index', {profileUpdated: true, email: email, user: name});
+						      return res.render('index', {profileUpdated: true, email: email, user: name, status: user.approved});
 						     
 						    });
 	      				}
@@ -56,29 +57,75 @@ module.exports = function(app) {
 
 	app.post('/', function (req, res, next) {
 		// console.log(req.params.data);
-		console.log(JSON.stringify(req.body.id));
+		console.log('body: ' + JSON.stringify(req.body.Id));
 
-		// Profile.findOne({ userId: req.session.userId })
-	 //    .exec(function (err, user) {
-	 //      if (err) {
-	 //        return callback(err)
-	 //      } else if (!user) {
-	 //        var err = new Error('User not found.');
-	 //        err.status = 401;
-	 //        return callback(err);
-	 //      }
-	 //      var name = user.lastname + " " + user.firstname;
-	 //      return res.render('index', {profileUpdated: true, email: email, user: name});
-	     
-	 //    });
+		var Id = req.body.Id;
+		Profile.findOneAndUpdate({userId: Id}, {$set:{approved:req.body.status}}, {new: true}, (err, doc) => {
+    		if (err) {
+        		console.log("Something wrong when updating data!");
+    		}
 
-		// var approvalData = {};
+    		// console.log(doc);
+    		res.redirect('/');
+		});
 
-		return res.redirect('/');
+		// var approvalData = {};	
 	});
 
 	app.get('/print-clearance', function (req, res, next) {
-		return res.redirect('/');
+
+		var email = req.session.userEmail;
+
+		User.findById(req.session.userId).exec(function (error, user) {
+	      	if (error) {
+	      		console.log(error);
+	      		res.redirect('/login');
+	       		return next();
+	     	} else {
+	        	if (user === null) {
+	          		var err = new Error('Not authorized! Go back!');
+	          		err.status = 400;
+	          		console.log(err);
+
+	          		res.redirect('/login');
+
+	         		return next();
+	        	} else {
+	        		// console.log(profileData);
+
+	          		Profile.authenticate(req.session.userId, function (error, profileUserId) {
+	          			var email = req.session.userEmail;
+	      				if (error || !profileUserId) {
+	      					Profile.find({},'userId firstname lastname department approved', function(err, profileData){
+	        					if (err) {
+	        						console.log("profileData could not be fetched");
+	        						return next();
+	        					}
+
+	        					return res.render('index', {profileUpdated: false, email: email, profileData: profileData, user: 'Admin', title: "Dashboard"});
+	        				});	
+	      				} else {
+
+	      					Profile.findOne({ userId: req.session.userId })
+						    .exec(function (err, user) {
+								if (err) {
+									return callback(err) 
+						    	} else if (user.approved !== 'approved') {
+						        	return res.redirect('/');
+						      	} else {
+						      		var name = user.lastname + " " + user.firstname;
+						      		return res.render('screening-cert', {profileUpdated: true, email: email, user: name});
+						      	}
+						     
+						    });
+
+	      				}
+      				});
+	        	}	
+	      	}
+    	});
+
+		
 	});
 
 	// app.post("/todo", urlencodedParser, function(req, res) {
